4061f951c384bc90dc851b7516c02a64bfad696b
Benjamin Chalmers <benj@minchalme.rs>
2018-01-22 22:11:45 +0000

    Added roles

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cd ./proposal
 pandoc -f markdown --template=../pandoc/new.latex HEAD.md \
 01-OUTLINE.md \
-02-MEETINGS.md -o ../Manifesto.pdf
+02-MEETINGS.md \
+03-STRUCTURE.md -o ../Manifesto.pdf
